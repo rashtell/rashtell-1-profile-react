6e2d8dc947bcbc94bab0fa7b9da7ce938c5baafa
Abdulrasheed Tella <rashtell2@gmail.com>
2020-06-09 16:17:36 +0100

    // fetchs: affects serv n portfo (disappears)

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,6 @@ import { render } from 'react-dom';
 import { Body } from './components/Body';
 
 
-console.log(`
-
-    This is a React Website 
-    =======================
-
-    Enjoy surfing 
-
-    `);
-
 render(
     <Body />, document.getElementById('root')
 )
